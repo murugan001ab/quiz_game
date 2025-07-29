fea5745fdb455a644492b4f789201f4d2cd1b53f
Murugan <105478973+murugan001ab@users.noreply.github.com>
2025-07-29 20:14:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -75,9 +75,9 @@ function App() {
             </ProtectedRoute>
           } />
           <Route path="/admin/start-quiz" element={
-            <ProtectedRoute>
+         
               <QuizStartPage />
-            </ProtectedRoute>
+           
           } />
          
           
